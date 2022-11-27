8f5abd5e6629fa331fa29b800abed6c51cfa2cb1
Raul Camilo Martin <raul.martin@rappi.com>
2022-11-26 19:05:20 -0500

    feat(EC2-Scripts): Ec2 scripts for use in aws

---
+++ scripts/ec2-preparation.sh
@@ -0,0 +1,7 @@
+sudo yum update -y
+sudo amazon-linux-extras install docker
+sudo service docker start
+sudo yum install git -y
+git clone https://github.com/rcamilo1526/globant_test.git
+sudo docker build -t data_api:0.1 .
+sudo docker run -d  -p 8000:8000 --name my-api data_api:0.1
---
+++ scripts/post_file.sh
@@ -0,0 +1,5 @@
+curl \
+  -F "username=camilo" \
+  -F "password=secret" \
+  -F "file=@/Users/raul.martin/Documents/globant_test/data/jobs.csv" \
+  http://44.203.244.150:8000/upload
