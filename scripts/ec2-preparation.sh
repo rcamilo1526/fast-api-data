132c3af3578e08c8807c30d06b582cb3f8b5a529
Raul Camilo Martin <raul.martin@rappi.com>
2022-11-27 20:30:51 -0500

    Change start ec2 instance script

--- scripts/ec2-preparation.sh
+++ scripts/ec2-preparation.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 sudo yum update -y
 sudo amazon-linux-extras install docker
 sudo service docker start
 sudo yum install git -y
-git clone https://github.com/rcamilo1526/globant_test.git
+git clone https://github.com/rcamilo1526/fast-api-data.git
+cd fast-api-data
 sudo docker build -t data_api:0.1 .
-sudo docker run -d  -p 8000:8000 --name my-api data_api:0.1
-docker run -p 8000:8000 --name my-api data_api:0.1
+nohup docker run -p 8000:8000 --name my-api data_api:0.1 > my.log 2>&1 &
