95a7700e48d7d68948010eba5e891ad000807b31
Raul Camilo Martin <raul.martin@rappi.com>
2022-11-29 21:31:40 -0500

    fix(Update-data-query): Change update query data to parquet

--- sql/copy_data.sql
+++ sql/copy_data.sql
@@ -1,13 +1,12 @@
--- TABLE UPLOAD
 COPY HIRED_EMPLOYEES FROM 
-'s3://company-data-{account_id}/data/hired_employees.csv'
+'s3://company-data-{account_id}/data/hired_employees.parquet'
 iam_role 'arn:aws:iam::{account_id}:role/SpectrumRole'
-CSV;
+PARQUET;
 COPY JOBS FROM 
-'s3://company-data-{account_id}/data/jobs.csv'
+'s3://company-data-{account_id}/data/jobs.parquet'
 iam_role 'arn:aws:iam::{account_id}:role/SpectrumRole'
-CSV;
+PARQUET;
 COPY DEPARTMENTS FROM 
-'s3://company-data-{account_id}/data/departments.csv'
+'s3://company-data-{account_id}/data/departments.parquet'
 iam_role 'arn:aws:iam::{account_id}:role/SpectrumRole'
-CSV;
+PARQUET;
