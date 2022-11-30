433b6629f113b2c850c8ac07a6f1b45a4f124cc8
Raul Camilo Martin <raul.martin@rappi.com>
2022-11-29 20:22:59 -0500

    refactor(data_dict): drop unnecesary dict

--- app/templates/data_dict.py
+++ app/templates/data_dict.py
@@ -1,27 +1,5 @@
 """Data dictionary."""
 
-table_dict_p = {'hired_employees': ({'column': 'id',
-                                     'type': 'INTEGER'},
-                                    {'column': 'name',
-                                     'type': 'STRING'},
-                                    {'column': 'datetime',
-                                     'type': 'ISO_DATE'},
-                                    {'column': 'department_id',
-                                     'type': 'INTEGER'},
-                                    {'column': 'job_id',
-                                     'type': 'INTEGER'}
-                                    ),
-                'departments': ({'column': 'id',
-                                 'type': 'INTEGER'},
-                                {'column': 'department',
-                                 'type': 'STRING'}
-                                ),
-                'jobs': ({'column': 'id',
-                          'type': 'INTEGER'},
-                         {'column': 'job',
-                          'type': 'STRING'}
-                         )
-                }
 table_dict = {'hired_employees': {'id': 'INTEGER',
                                   'name': 'STRING',
                                   'datetime': 'ISO_DATE',
